1d7b840c0be0805b46f35930885dc27d054304a4
vernini <vernini80@gmail.com>
2020-12-30 12:07:55 +0900

    test

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
+# coding:utf-8
+
 import os
 
-print 'test'
+# class 이해하기
+
+class classA():
+    def __init__(self):
+        print 'start class'
+
+    def push_command(self, ver):
+        print 'push %s' %ver
+
+    def run_command(self):
+        print 'run'
+
+
+classA().push_command('button')
